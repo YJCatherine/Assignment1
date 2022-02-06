4b5d41e35badd59b7bf069c7f6fda869f5cc5568
YJCatherine <98239423+YJCatherine@users.noreply.github.com>
2022-02-06 11:43:34 -0800

    Update Assignment2.java

--- Assignment2/Assignment2.java
+++ Assignment2/Assignment2.java
@@ -161,7 +161,7 @@ public class Assignment2 {
             i++;
         }
 
-        while (i < intervals.length && intervals[i][0] <= newInterval[0]){
+        while (i < intervals.length && intervals[i][0] <= newInterval[1]){
             newInterval[0] = Math.min(intervals[i][0], newInterval[0]);
             newInterval[1] = Math.max(intervals[i][1], newInterval[1]);
             i++;
