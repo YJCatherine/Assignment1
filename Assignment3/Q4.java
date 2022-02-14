8abcd9af399d3d6f8755fa3a228ee19c82af5a72
YJCatherine <98239423+YJCatherine@users.noreply.github.com>
2022-02-14 02:48:07 -0800

    Update Q4.java

--- Assignment3/Q4.java
+++ Assignment3/Q4.java
@@ -1,5 +1,14 @@
 package Assignment3;
-
+/*
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 public class Q4 {
     public void reorderList(ListNode head) {
         int len = 0;
